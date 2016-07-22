c410049758a40fee126eda6db3cf190abb22a273
Didier Villevalois <ptitjes@free.fr>
2016-07-22 19:43:59 +0200

    [AST] Test for states classes' kind

--- src/main/java/org/jlato/bootstrap/tests/TreesKindTest.java
+++ src/main/java/org/jlato/bootstrap/tests/TreesKindTest.java
@@ -21,6 +21,7 @@ public class TreesKindTest extends TestPattern {
 	@Override
 	protected void contributeImports(ImportManager importManager) {
 		importManager.addImportByName(qualifiedName("org.jlato.tree.Kind"));
+		importManager.addImportByName(qualifiedName("org.jlato.internal.td.TDTree"));
 	}
 
 	@Override
@@ -35,6 +36,16 @@ public class TreesKindTest extends TestPattern {
 						fieldAccessExpr(descriptor.name).withScope(name("Kind")),
 						methodInvocationExpr(name("kind")).withScope(
 								factoryCall(descriptor, importManager)
+						)
+				),
+				junitAssert("assertEquals",
+						fieldAccessExpr(descriptor.name).withScope(name("Kind")),
+						methodInvocationExpr(name("kind")).withScope(
+								methodInvocationExpr(name("stateOf")).withScope(
+										name("TDTree")
+								).withArgs(listOf(
+										factoryCall(descriptor, importManager)
+								))
 						)
 				)
 		);
