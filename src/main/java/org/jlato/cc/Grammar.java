002bfd0673c555d31a31953a995f08d7fc7fc5e9
Didier Villevalois <ptitjes@free.fr>
2016-09-14 21:28:19 +0200

    Do not memoize ExplicitConstructorInvocations

--- src/main/java/org/jlato/cc/Grammar.java
+++ src/main/java/org/jlato/cc/Grammar.java
@@ -1466,7 +1466,7 @@ public class Grammar {
 									stmt("return dress(SExplicitConstructorInvocationStmt.make(ensureNotNull(typeArgs), isThis, optionOf(expr), args));").build()
 							))
 					)
-			).memoizeMatches(),
+			),
 			production("Statements", type("BUTree<SNodeList>").build(),
 					listOf(
 							param("boolean inConstructor").build()
