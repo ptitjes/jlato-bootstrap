2246091e6e8bc7e7130e34bc7104eb531f737ba2
Didier Villevalois <ptitjes@free.fr>
2016-12-24 03:34:04 +0100

    [CC2] Make LL1 decisions use bit operations

--- src/main/java/org/jlato/cc/ParserPattern2.java
+++ src/main/java/org/jlato/cc/ParserPattern2.java
@@ -1,18 +1,22 @@
 package org.jlato.cc;
 
-import com.github.andrewoma.dexx.collection.*;
 import org.jlato.bootstrap.descriptors.TreeClassDescriptor;
 import org.jlato.bootstrap.util.ImportManager;
 import org.jlato.bootstrap.util.TypePattern;
 import org.jlato.cc.grammar.*;
+import org.jlato.internal.td.expr.TDLiteralExpr;
 import org.jlato.tree.Kind;
 import org.jlato.tree.NodeList;
+import org.jlato.tree.NodeOption;
 import org.jlato.tree.Trees;
 import org.jlato.tree.decl.*;
 import org.jlato.tree.expr.*;
+import org.jlato.tree.name.Name;
 import org.jlato.tree.stmt.ExpressionStmt;
+import org.jlato.tree.stmt.IfStmt;
 import org.jlato.tree.stmt.Stmt;
 import org.jlato.tree.stmt.SwitchCase;
+import org.jlato.tree.type.Primitive;
 import org.jlato.tree.type.Type;
 
 import java.util.*;
@@ -444,6 +448,7 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 
 		NodeList<Stmt> stmts = emptyList();
 		stmts = stmts.appendAll(production.declarations);
+		stmts = stmts.append(tokenVarDeclaration());
 		stmts = stmts.appendAll(parseStatementsFor(production.symbol, production.symbol, production.location(), production.hintParams, false));
 
 		// Add push/pop callStack calls
@@ -469,6 +474,7 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 				if (expansion.children.size() == 1 && expansion.children.get(0).kind == GExpansion.Kind.Choice) {
 					stmts = stmts.appendAll(parseStatementsForChildren(symbol, namePrefix, location, hintParams, true));
 				} else {
+					stmts = stmts.append(tokenVarUpdate());
 					stmts = stmts.append(ifStmt(
 							makeKleeneCondition(namePrefix, location, hintParams),
 							blockStmt().withStmts(parseStatementsForChildren(symbol, namePrefix, location, hintParams, false))
@@ -477,15 +483,16 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 				break;
 			}
 			case ZeroOrMore: {
+				stmts = stmts.append(tokenVarUpdate());
 				stmts = stmts.append(whileStmt(
 						makeKleeneCondition(namePrefix, location, hintParams),
-						blockStmt().withStmts(parseStatementsForChildren(symbol, namePrefix, location, hintParams, false))
+						blockStmt().withStmts(parseStatementsForChildren(symbol, namePrefix, location, hintParams, false).append(tokenVarUpdate()))
 				));
 				break;
 			}
 			case OneOrMore: {
 				stmts = stmts.append(doStmt(
-						blockStmt().withStmts(parseStatementsForChildren(symbol, namePrefix, location, hintParams, false)),
+						blockStmt().withStmts(parseStatementsForChildren(symbol, namePrefix, location, hintParams, false).append(tokenVarUpdate())),
 						makeKleeneCondition(namePrefix, location, hintParams)
 				));
 				break;
@@ -493,35 +500,33 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 			case Choice: {
 				List<Set<String>> ll1DecisionTerminals = computeChoiceLL1DecisionTerminals(location);
 				if (ll1DecisionTerminals != null) {
-					Expr selector = getTokenKind(literalExpr(0));
 
-					NodeList<SwitchCase> cases = emptyList();
+					NodeList<IfStmt> cases = emptyList();
 
 					int count = 1;
 					for (GLocation child : location.allChildren()) {
-						List<Expr> terminals = prefixedAndOrderedConstants(ll1DecisionTerminals.get(count - 1));
+						Set<String> terminals = ll1DecisionTerminals.get(count - 1);
 
 						// Produce statements
 						String name = namePrefix + "_" + count++;
 						NodeList<Stmt> caseStmts = parseStatementsFor(symbol, name, child, hintParams, optional);
-						if (caseStmts.last().kind() != Kind.ReturnStmt) caseStmts = caseStmts.append(breakStmt());
 
-						// Produce labels
-						int i = 0;
-						for (; i < terminals.size() - 1; i++) {
-							cases = cases.append(switchCase().withLabel(terminals.get(i)));
-						}
-						cases = cases.append(switchCase().withLabel(terminals.get(i)).withStmts(caseStmts));
+						// Produce case
+						cases = cases.append(ifStmt(matchExpression(terminals), blockStmt().withStmts(caseStmts)));
 					}
 
-					if (!optional) {
-						cases = cases.append(switchCase().withStmts(listOf(throwStmt(
-								methodInvocationExpr(name("produceParseException"))
-										.withArgs(listOf(prefixedAndOrderedConstants(firstTerminalsOf(location))))
-						))));
-					}
+					NodeOption<Stmt> stmt = cases.foldRight(
+							optional ? Trees.<Stmt>none() : some(throwStmt(
+									methodInvocationExpr(name("produceParseException"))
+											.withArgs(listOf(prefixedAndOrderedConstants(firstTerminalsOf(location))))
+							)),
+							(ifStmt, elseStmt) -> some(ifStmt.withElseStmt(elseStmt))
+					);
 
-					stmts = stmts.append(switchStmt(selector).withCases(cases));
+					stmts = stmts.appendAll(listOf(
+							tokenVarUpdate(),
+							stmt.get()
+					));
 				} else {
 					Expr selector = predict(namePrefix, hintParams);
 
@@ -580,6 +585,97 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 		return stmts;
 	}
 
+	private static final Name TOKEN_VAR_NAME = name("__token");
+
+	private Stmt tokenVarDeclaration() {
+		return expressionStmt(variableDeclarationExpr(
+				localVariableDecl(primitiveType(Primitive.Int)).withVariables(listOf(
+						variableDeclarator(variableDeclaratorId(TOKEN_VAR_NAME))
+				))
+		));
+	}
+
+	private Stmt tokenVarUpdate() {
+		return expressionStmt(assignExpr(TOKEN_VAR_NAME, AssignOp.Normal, getTokenKind(literalExpr(0))));
+	}
+
+	private Expr matchExpression(Collection<String> terminals) {
+		return matchExpression(prefixedAndOrderedConstants(terminals));
+	}
+
+	private Expr matchExpression(List<Expr> terminalNames) {
+		// TODO Have a better heuristic to choose between explicit matches and bit operations
+		if (terminalNames.size() == 1)
+			return matchExpression(terminalNames.get(0));
+		else if (terminalNames.size() == 2)
+			return binaryExpr(matchExpression(terminalNames.get(0)), BinaryOp.Or, matchExpression(terminalNames.get(1)));
+		else {
+			Expr test = null;
+			Expr mask = null;
+			int firstValue = -1;
+
+			for (Expr terminal : terminalNames) {
+				int value = terminalTable.get(terminal);
+
+				if (firstValue == -1) firstValue = value;
+				if (value >= firstValue + 64) {
+					Expr thisTest = testFor(mask, firstValue);
+
+					if (test == null) test = thisTest;
+					else test = binaryExpr(test, BinaryOp.Or, thisTest);
+
+					firstValue = value;
+					mask = null;
+				}
+
+				Expr thisMask = maskFor(terminal, firstValue);
+				if (mask == null) mask = thisMask;
+				else mask = binaryExpr(mask, BinaryOp.BinOr, thisMask);
+			}
+
+			if (mask != null) {
+				Expr thisTest = testFor(mask, firstValue);
+
+				if (test == null) test = thisTest;
+				else test = binaryExpr(parenthesizedExpr(test), BinaryOp.Or, parenthesizedExpr(thisTest));
+			}
+
+			return test;
+		}
+	}
+
+	private Expr testFor(Expr mask, int firstValue) {
+		return binaryExpr(
+				binaryExpr(
+						parenthesizedBinExpr(
+								binaryExpr(TOKEN_VAR_NAME, BinaryOp.Minus, literalExpr(firstValue)),
+								BinaryOp.BinAnd,
+								unaryExpr(UnaryOp.Inverse, literalExpr(63))
+						),
+						BinaryOp.Equal,
+						literalExpr(0)
+				),
+				BinaryOp.And,
+				binaryExpr(
+						parenthesizedBinExpr(maskFor(TOKEN_VAR_NAME, firstValue), BinaryOp.BinAnd, parenthesizedExpr(mask)),
+						BinaryOp.NotEqual,
+						literalExpr(0)
+				)
+		);
+	}
+
+	private Expr maskFor(Expr name, int firstValue) {
+		return binaryExpr(literalExpr(1L), BinaryOp.LeftShift, binaryExpr(name, BinaryOp.Minus, literalExpr(firstValue)));
+	}
+
+	private Expr parenthesizedBinExpr(Expr left, BinaryOp op, Expr right) {
+		return parenthesizedExpr(binaryExpr(left, op, right));
+	}
+
+	private Expr matchExpression(Expr terminalName) {
+		return binaryExpr(TOKEN_VAR_NAME, BinaryOp.Equal, terminalName);
+	}
+
 	private NodeList<Stmt> parseStatementsForChildren(String symbol, String namePrefix, GLocation location, NodeList<FormalParameter> hintParams, boolean optional) {
 		int count = 1;
 		NodeList<Stmt> stmts = emptyList();
@@ -614,10 +710,10 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 		return terminalSets;
 	}
 
-	private BinaryExpr makeKleeneCondition(String namePrefix, GLocation location, NodeList<FormalParameter> hintParams) {
+	private Expr makeKleeneCondition(String namePrefix, GLocation location, NodeList<FormalParameter> hintParams) {
 		List<String> ll1DecisionTerminals = computeKleeneLL1DecisionTerminals(location);
 		return ll1DecisionTerminals != null ?
-				binaryExpr(match(ll1DecisionTerminals, literalExpr(0)), BinaryOp.NotEqual, literalExpr(-1)) :
+				matchExpression(ll1DecisionTerminals) :
 				binaryExpr(predict(namePrefix, hintParams), BinaryOp.Equal, literalExpr(1));
 	}
 
@@ -646,8 +742,9 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 		return fieldAccessExpr(name("kind")).withScope(methodInvocationExpr(name("getToken")).withArgs(listOf(lookahead)));
 	}
 
-	private MethodInvocationExpr match(List<String> tokens, Expr lookahead) {
-		return methodInvocationExpr(name("match")).withArgs(listOf(prefixedAndOrderedConstants(tokens)).prepend(lookahead));
+	private Expr match(Collection<String> tokens, Expr lookahead) {
+		List<Expr> terminals = prefixedAndOrderedConstants(tokens);
+		return methodInvocationExpr(name("match")).withArgs(listOf(terminals).prepend(lookahead));
 	}
 
 	private Stmt pushCallStack(String name) {
@@ -663,8 +760,8 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 	private List<Expr> prefixedAndOrderedConstants(Collection<String> tokens) {
 		return tokens.stream()
 				.filter(t -> t != null)
-				.sorted()
-				.map(t -> prefixedConstant(t))
+				.map(this::prefixedConstant)
+				.sorted(terminalComparator)
 				.collect(Collectors.toList());
 	}
 
@@ -676,5 +773,139 @@ public class ParserPattern2 extends TypePattern.OfClass<TreeClassDescriptor[]> {
 		GContinuations c = new GContinuations(location, productions, false);
 		c.next();
 		return c.terminals();
+	}
+
+	private Comparator<Expr> terminalComparator = new Comparator<Expr>() {
+		@Override
+		public int compare(Expr o1, Expr o2) {
+			return terminalTable.get(o1).compareTo(terminalTable.get(o2));
+		}
+	};
+
+	private Map<Expr, Integer> terminalTable = new HashMap<>();
+
+	private void addTerminal(String name, int value) {
+		terminalTable.put(prefixedConstant(name), value);
+	}
+
+	{
+		addTerminal("EOF", 0);
+		addTerminal("WHITESPACE", 1);
+		addTerminal("NEWLINE", 2);
+		addTerminal("SINGLE_LINE_COMMENT", 3);
+		addTerminal("JAVA_DOC_COMMENT", 6);
+		addTerminal("MULTI_LINE_COMMENT", 7);
+		addTerminal("ABSTRACT", 9);
+		addTerminal("ASSERT", 10);
+		addTerminal("BOOLEAN", 11);
+		addTerminal("BREAK", 12);
+		addTerminal("BYTE", 13);
+		addTerminal("CASE", 14);
+		addTerminal("CATCH", 15);
+		addTerminal("CHAR", 16);
+		addTerminal("CLASS", 17);
+		addTerminal("CONST", 18);
+		addTerminal("CONTINUE", 19);
+		addTerminal("DEFAULT", 20);
+		addTerminal("DO", 21);
+		addTerminal("DOUBLE", 22);
+		addTerminal("ELSE", 23);
+		addTerminal("ENUM", 24);
+		addTerminal("EXTENDS", 25);
+		addTerminal("FALSE", 26);
+		addTerminal("FINAL", 27);
+		addTerminal("FINALLY", 28);
+		addTerminal("FLOAT", 29);
+		addTerminal("FOR", 30);
+		addTerminal("GOTO", 31);
+		addTerminal("IF", 32);
+		addTerminal("IMPLEMENTS", 33);
+		addTerminal("IMPORT", 34);
+		addTerminal("INSTANCEOF", 35);
+		addTerminal("INT", 36);
+		addTerminal("INTERFACE", 37);
+		addTerminal("LONG", 38);
+		addTerminal("NATIVE", 39);
+		addTerminal("NEW", 40);
+		addTerminal("NULL", 41);
+		addTerminal("PACKAGE", 42);
+		addTerminal("PRIVATE", 43);
+		addTerminal("PROTECTED", 44);
+		addTerminal("PUBLIC", 45);
+		addTerminal("RETURN", 46);
+		addTerminal("SHORT", 47);
+		addTerminal("STATIC", 48);
+		addTerminal("STRICTFP", 49);
+		addTerminal("SUPER", 50);
+		addTerminal("SWITCH", 51);
+		addTerminal("SYNCHRONIZED", 52);
+		addTerminal("THIS", 53);
+		addTerminal("THROW", 54);
+		addTerminal("THROWS", 55);
+		addTerminal("TRANSIENT", 56);
+		addTerminal("TRUE", 57);
+		addTerminal("TRY", 58);
+		addTerminal("VOID", 59);
+		addTerminal("VOLATILE", 60);
+		addTerminal("WHILE", 61);
+		addTerminal("LONG_LITERAL", 62);
+		addTerminal("INTEGER_LITERAL", 63);
+		addTerminal("FLOAT_LITERAL", 68);
+		addTerminal("DOUBLE_LITERAL", 69);
+		addTerminal("CHARACTER_LITERAL", 78);
+		addTerminal("STRING_LITERAL", 79);
+		addTerminal("LPAREN", 80);
+		addTerminal("RPAREN", 81);
+		addTerminal("LBRACE", 82);
+		addTerminal("RBRACE", 83);
+		addTerminal("LBRACKET", 84);
+		addTerminal("RBRACKET", 85);
+		addTerminal("SEMICOLON", 86);
+		addTerminal("COMMA", 87);
+		addTerminal("DOT", 88);
+		addTerminal("AT", 89);
+		addTerminal("ASSIGN", 90);
+		addTerminal("LT", 91);
+		addTerminal("BANG", 92);
+		addTerminal("TILDE", 93);
+		addTerminal("HOOK", 94);
+		addTerminal("COLON", 95);
+		addTerminal("EQ", 96);
+		addTerminal("LE", 97);
+		addTerminal("GE", 98);
+		addTerminal("NE", 99);
+		addTerminal("SC_OR", 100);
+		addTerminal("SC_AND", 101);
+		addTerminal("INCR", 102);
+		addTerminal("DECR", 103);
+		addTerminal("PLUS", 104);
+		addTerminal("MINUS", 105);
+		addTerminal("STAR", 106);
+		addTerminal("SLASH", 107);
+		addTerminal("BIT_AND", 108);
+		addTerminal("BIT_OR", 109);
+		addTerminal("XOR", 110);
+		addTerminal("REM", 111);
+		addTerminal("LSHIFT", 112);
+		addTerminal("PLUSASSIGN", 113);
+		addTerminal("MINUSASSIGN", 114);
+		addTerminal("STARASSIGN", 115);
+		addTerminal("SLASHASSIGN", 116);
+		addTerminal("ANDASSIGN", 117);
+		addTerminal("ORASSIGN", 118);
+		addTerminal("XORASSIGN", 119);
+		addTerminal("REMASSIGN", 120);
+		addTerminal("LSHIFTASSIGN", 121);
+		addTerminal("RSIGNEDSHIFTASSIGN", 122);
+		addTerminal("RUNSIGNEDSHIFTASSIGN", 123);
+		addTerminal("ELLIPSIS", 124);
+		addTerminal("ARROW", 125);
+		addTerminal("DOUBLECOLON", 126);
+		addTerminal("RUNSIGNEDSHIFT", 127);
+		addTerminal("RSIGNEDSHIFT", 128);
+		addTerminal("GT", 129);
+		addTerminal("NODE_VARIABLE", 130);
+		addTerminal("NODE_LIST_VARIABLE", 131);
+		addTerminal("IDENTIFIER", 132);
 	}
 }
